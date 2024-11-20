d9a9a6d6ea00d451062ab364269ccef27eb4ea3f
churin <wl00796865@gmail.com>
2024-11-20 17:21:52 +0800

    if else elseif

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,19 @@
+<script>
+//Vue 2
+export default {
+  // data function, not object
+  data() {
+    return {
+      name: 'Donald Trump',
+      status: 'pending',
+    }
+  },
+}
+</script>
+
 <template>
-  <h1>Hello World</h1>
+  <h1>{{ name }}</h1>
+  <p v-if="status === 'active'">User Is Active</p>
+  <p v-else-if="status === 'pending'">User Is Pending</p>
+  <p v-else>User Is Not Active</p>
 </template>
