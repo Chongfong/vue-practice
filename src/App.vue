5c117c1e9e9f938bc36e63581c8ddea15c24dcd6
churin <wl00796865@gmail.com>
2024-11-20 17:24:39 +0800

    loop datas

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,7 @@ export default {
     return {
       name: 'Donald Trump',
       status: 'pending',
+      tasks: ['Task One', 'Task Two', 'Task Three'],
     }
   },
 }
@@ -16,4 +17,9 @@ export default {
   <p v-if="status === 'active'">User Is Active</p>
   <p v-else-if="status === 'pending'">User Is Pending</p>
   <p v-else>User Is Not Active</p>
+
+  <h3>Tasks</h3>
+  <ul>
+    <li v-for="task in tasks" v-bind:key="task">{{ task }}</li>
+  </ul>
 </template>
