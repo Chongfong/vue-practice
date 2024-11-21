67d8e5d29ff53b5994c7a01e9caadcc3b2db864d
churin <wl00796865@gmail.com>
2024-11-21 15:11:23 +0800

    homecard component with slot

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 import Navbar from './components/Navbar.vue'
 import Hero from './components/Hero.vue'
+import HomeCards from './components/HomeCards.vue'
 </script>
 
 <template>
   <Navbar />
   <Hero title="Vue Jobs" subtitle="Find the Vue job that fits your skills and needs" />
+  <HomeCards />
 </template>
---
+++ src/components/Card.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { defineProps } from 'vue'
+defineProps({
+  bgColor: {
+    type: String,
+    default: 'bg-gray-100',
+  },
+})
+</script>
+
+<template>
+  <div :class="`${bgColor} p-6 rounded-lg shadow-md`">
+    <!-- like {children} in React -->
+    <slot></slot>
+  </div>
+</template>
