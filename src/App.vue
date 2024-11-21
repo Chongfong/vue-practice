cfceb70cad9a55d0885c3ae686c4f8a76b54921c
churin <wl00796865@gmail.com>
2024-11-21 15:53:47 +0800

    job listing limit and showbutton

--- src/App.vue
+++ src/App.vue
@@ -9,5 +9,6 @@ import JobList from './components/JobList.vue'
   <Navbar />
   <Hero title="Vue Jobs" subtitle="Find the Vue job that fits your skills and needs" />
   <HomeCards />
-  <JobList />
+  <!-- with :, accept numbers, variables, dynamic data -->
+  <JobList :limit="3" :showMore="true" />
 </template>
--- src/components/JobList.vue
+++ src/components/JobList.vue
@@ -1,9 +1,16 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, defineProps } from 'vue'
 import Job from './Job.vue'
 import jobsObject from '../jobs.json'
 // remember to use ref to wrap data, or it will get empty object
 const jobs = ref(jobsObject.jobs)
+defineProps({
+  limit: Number,
+  showMore: {
+    type: Boolean,
+    default: false,
+  },
+})
 </script>
 
 <template>
@@ -11,8 +18,15 @@ const jobs = ref(jobsObject.jobs)
     <div class="container-xl lg:container m-auto">
       <h2 class="text-3xl font-bold text-green-500 mb-6 text-center">Browse Jobs</h2>
       <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
-        <Job v-for="job in jobs" :key="job.id" :job="job" />
+        <Job v-for="job in jobs.slice(0, limit || jobs.length)" :key="job.id" :job="job" />
       </div>
     </div>
+  </section>
+  <section v-if="showMore" class="m-auto max-w-lg my-10 px-6">
+    <a
+      href="/jobs"
+      class="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
+      >View All Jobs</a
+    >
   </section>
 </template>
