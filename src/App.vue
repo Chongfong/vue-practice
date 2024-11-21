9cf774cda22016277ab9f6ffa44cb50a8237cee3
churin <wl00796865@gmail.com>
2024-11-21 15:01:23 +0800

    use props to build hero component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup>
 import Navbar from './components/Navbar.vue'
+import Hero from './components/Hero.vue'
 </script>
 
 <template>
   <Navbar />
+  <Hero title="Vue Jobs" subtitle="Find the Vue job that fits your skills and needs" />
 </template>
