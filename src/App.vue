eccac6e5d3e6eca8a0596cd1794c98c8687543fc
churin <wl00796865@gmail.com>
2024-11-21 15:38:40 +0800

    list jobs

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,12 @@
 import Navbar from './components/Navbar.vue'
 import Hero from './components/Hero.vue'
 import HomeCards from './components/HomeCards.vue'
+import JobList from './components/JobList.vue'
 </script>
 
 <template>
   <Navbar />
   <Hero title="Vue Jobs" subtitle="Find the Vue job that fits your skills and needs" />
   <HomeCards />
+  <JobList />
 </template>
