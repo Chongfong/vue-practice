17d1d8d0e2739fde31a72fff19efd3254c787232
churin <wl00796865@gmail.com>
2024-11-22 10:47:45 +0800

    router settings:home

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,9 @@
 <script setup>
 import Navbar from './components/Navbar.vue'
-import Hero from './components/Hero.vue'
-import HomeCards from './components/HomeCards.vue'
-import JobList from './components/JobList.vue'
+import { RouterView } from 'vue-router'
 </script>
 
 <template>
   <Navbar />
-  <Hero title="Vue Jobs" subtitle="Find the Vue job that fits your skills and needs" />
-  <HomeCards />
-  <!-- with :, accept numbers, variables, dynamic data -->
-  <JobList :limit="3" :showMore="true" />
+  <RouterView />
 </template>
---
+++ src/views/Home.vue
@@ -0,0 +1,12 @@
+<script setup>
+import Hero from '@/components/Hero.vue'
+import HomeCards from '@/components/HomeCards.vue'
+import JobList from '@/components/JobList.vue'
+</script>
+
+<template>
+  <Hero title="Vue Jobs" subtitle="Find the Vue job that fits your skills and needs" />
+  <HomeCards />
+  <!-- with :, accept numbers, variables, dynamic data -->
+  <JobList :limit="3" :showMore="true" />
+</template>
